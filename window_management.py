cf393f0bbd4b2590082826e80f1e546f09e80248
Damjan Roskar <damjan.roskar@statsperform.com>
2023-04-14 00:01:46 +0200

    Custom launch names

---
+++ window_management.py
@@ -0,0 +1,50 @@
+import os
+import talon
+import subprocess
+from talon import Context, Module, ui
+
+mod = Module()
+mod.list("installed_apps", desc="Installed apps")
+
+ctx_default = Context ()
+ctx_default.lists["user.installed_apps"] = {
+    "note": "C:\\Program Files (x86)\\Notepad++\\notepad++.exe",
+    "source": "C:\\Users\\Damjan.Roskar\\AppData\\Local\\SourceTree\\app-3.4.6\\SourceTree.exe"
+}
+
+@mod.action_class
+class Actions:
+    # def app_open(appId: str):
+    #     """Open an application by AppUserModel ID"""
+    #     print(appId)
+    #     cmd = f"explorer.exe shell:AppsFolder\\{appId}"
+    #     subprocess.Popen(cmd, shell=False)
+
+    def app_open(path: str):
+        """Open an application by path"""
+        print(path)
+        ui.launch(path=path)
+
+    def app_focus(path: str):
+        """Focus an application by path"""
+        app = get_running_app(path)
+        print(app)
+        switcher_focus_app(app)
+
+def switcher_focus_app(app: ui.App):
+    """Focus application and wait until switch is made"""
+    app.focus()
+    t1 = time.perf_counter()
+    while ui.active_app() != app:
+        if time.perf_counter() - t1 > 1:
+            raise RuntimeError(f"Can't focus app: {app.name}")
+        actions.sleep(0.1)
+
+def get_running_app(path: str) -> ui.App:
+        """Get the running app with `path`."""
+        for application in ui.apps(background=False):
+            print(application.name)
+            print(os.path.basename(application.exe))
+            if os.path.basename(application.exe) == os.path.basename(path):
+                return application
+        raise RuntimeError(f'App not running: "{path}"')
---
+++ window_management.talon
@@ -0,0 +1,4 @@
+os: windows
+-
+launch {user.installed_apps}: user.app_open(installed_apps)
+focus {user.installed_apps}: user.app_focus(installed_apps)
